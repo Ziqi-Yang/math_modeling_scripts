71d1d085aaf37de0b49149fd6a9db55ac5eeab94
Ziqi-Yang <mr.ziqiyang@gmail.com>
2022-01-21 22:35:21 +0800

    normal

---
+++ data_process/README.md
@@ -0,0 +1,33 @@
+# Data Process Helper
+
+## 注意事项
+请将`main.py`和`datas`、`result`目录放在一起，将需要处理的数据文件放在`datas`目录下。
+
+
+## 设置数据文件属性
+
+## 所有属性
+
+- `dataFileType`: `time-expand`,`time-in-column` 默认值 `time-expand`
+
+`time-expand`:
+![time-expand](demo/timeExpand.png)
+
+`time-in-column`:
+![time-in-column](demo/timeInColumn.png)
+
+- `timeColumn`: `$columnName`
+根据`dataFileType`设置此属性为不同的值:
+    - `time-expand`: `None` (直接回车即可,不要打`None`)
+    - `time-in-column`: `$columnName` (ps: 如果列名有重复，请按参考以下: `a`,`a.1`,`a.2`)
+
+### 示例
+
+#### World Bank 获取的文件设置
+
+![WorldBank](demo/WorldBank.png)
+
+```python
+
+```
+
--- data_process/main.py
+++ data_process/main.py
@@ -1,3 +1,34 @@
+from collections.abc import Iterable
+from compileall import compile_file
 import pandas as pd
+import pickle
+import sys
+import os
+from pyparsing import Combine
+from rich.console import Console
+from rich.prompt import Prompt,Confirm
 
-processFiles = []
+console = Console()
+
+rootPath = sys.path[0]
+dataFolder = os.path.join(rootPath,"datas")
+resultFolder = os.path.join(rootPath,"res")
+dataFilesNames = os.listdir(dataFolder)
+dataFiles = list(map(lambda x: os.path.join(dataFolder,x),dataFilesNames))
+# dataFileTypeFunc ={"Expand":}
+
+console.print(f"Here are data files in the 'datas' folder:\n{dataFilesNames} ")
+console.print("Please set suitable attribution for each data file:")
+
+dataFilesAttrs = {dataFileName: dict() for dataFileName in dataFilesNames}
+attrsChoice = {"dataFileType":["time-expand","time-in-column"],"timeColumn": None,} # single stands for no limit, but default is the word
+
+for dataFileName in dataFilesNames:
+    console.rule(f"[bold magenta]{dataFileName}[/]")
+    for attr in list(attrsChoice.keys()):
+        dataFilesAttrs[dataFileName][attr] = Prompt.ask(f"{attr}",default=attrsChoice[attr][0] if isinstance(attrsChoice[attr],Iterable) else attrsChoice[attr],choices=attrsChoice[attr] if isinstance(attrsChoice[attr],list) else None)
+console.clear()
+console.rule(f"[bold red]Attribution Viewer[/]")
+console.print(dataFilesAttrs)
+
+Confirm.ask("Has [bold red]problems[/] in the Attributions?")
